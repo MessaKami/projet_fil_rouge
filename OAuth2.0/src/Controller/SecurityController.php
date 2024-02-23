fdab37cb76e0b4557ffb34ef5b60059244c0aa98
MessaKami <78752191+MessaKami@users.noreply.github.com>
2024-02-23 01:04:01 +0100

    Auth Google fini

--- OAuth2.0/src/Controller/SecurityController.php
+++ OAuth2.0/src/Controller/SecurityController.php
@@ -4,16 +4,22 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+final class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response 
+    public const SCOPES = [
+        'google' => [],
+    ];
+
+    #[Route(path: '/login', name: 'auth_oauth_login', methods: ['GET'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // $user = new User();
         // $user->setEmail('johndoe@gmail.fr');
@@ -22,9 +28,9 @@ class SecurityController extends AbstractController
         // $em->persist($user);
         // $em->flush();
 
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -34,9 +40,26 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/logout', name: 'auth_oauth_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/oauth/connect/{service}', name: 'auth_oauth_connect', methods: ['GET'])]
+    public function connect(string $service, ClientRegistry $clientRegistry): RedirectResponse
+    {
+        if (! in_array($service, array_keys(self::SCOPES), true)) {
+            throw $this->createNotFoundException();
+        }
+
+        return $clientRegistry->getClient($service)->redirect(self::SCOPES[$service]);
+    }
+
+    public function check(): Response
+    {
+        return new Response(status: 200);
+    }
+
+    
 }
